12f00f0dcf6effbad8a52b86ce6e85fea45f9727
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2022-01-24 20:58:55 -0300

    feat: add routes fornecedores

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -1,12 +1,35 @@
 const router = require('express').Router();
 const TabelaFornecedor = require('./tabelaFornecedor');
+const Fornecedor = require('./Fornecedor');
 
-router.use('/api/fornecedores', async (req, res) => {
+router.get('/api/fornecedores', async (req, res) => {
   const results = await TabelaFornecedor.listar();
 
   res.send(
     JSON.stringify(results)
   );
 });
+
+router.post('/api/fornecedores', async (req, res) => {
+  const dados = req.body;
+  const fornecedor = new Fornecedor(dados);
+
+  await fornecedor.criar();
+
+  return res.send(JSON.stringify(fornecedor));
+})
+
+router.get('/api/fornecedores/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const fornecedor = new Fornecedor({ id });
+
+    await fornecedor.carregar();
+
+    return res.send(JSON.stringify(fornecedor));
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+})
 
 module.exports = router;
--- api/rotas/fornecedores/tabelaFornecedor.js
+++ api/rotas/fornecedores/tabelaFornecedor.js
@@ -2,5 +2,19 @@ const model = require('./modelTableFornecedor');
 module.exports = {
   listar() {
     return model.findAll();
+  },
+
+  inserir(fornecedor) {
+    return model.create(fornecedor);
+  },
+
+  async unique(id) {
+    const encontrado = await model.findOne({ where: { id: id } });
+
+    if (!encontrado) {
+      throw new Error('Fornecedor não encontrado');
+    }
+
+    return encontrado;
   }
 }
