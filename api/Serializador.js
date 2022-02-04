78721526b33d99ab6d679c1a10e780da663d094d
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2022-02-03 22:12:06 -0300

    feat: add xml accept

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,13 +1,29 @@
 const ValorNaoSuportado = require("./erros/ValorNaoSuportado");
+const jsontoxml = require("jsontoxml");
 
 class Serializador {
   json(dados) {
     return JSON.stringify(dados);
   }
 
+  xml(dados) {
+    let tag = this.tagSingular
+
+    if (Array.isArray(dados)) {
+      tag = this.tagPlural
+    }
+
+    return jsontoxml({ [tag]: dados });
+  }
+
   serializar(dados) {
+    dados = this.filtrar(dados);
     if (this.contentType === 'application/json') {
-      return this.json(this.filtrar(dados));
+      return this.json(dados);
+    }
+
+    if (this.contentType === 'application/xml') {
+      return this.xml(dados);
     }
 
     throw new ValorNaoSuportado(this.contentType);
@@ -41,6 +57,8 @@ class SerializadorErro extends Serializador {
     super();
     this.contentType = contentType;
     this.camposPublicos = ['success', 'error'].concat(camposExtras || []);
+    this.tagSingular = 'fornecedor';
+    this.tagPlural = 'fornecedores';
   }
 }
 
@@ -49,6 +67,8 @@ class SerializadorFornecedor extends Serializador {
     super();
     this.contentType = contentType;
     this.camposPublicos = ['id', 'empresa', 'categoria'].concat(camposExtras || []);
+    this.tagSingular = 'fornecedor';
+    this.tagPlural = 'fornecedores';
   }
 }
 
@@ -56,5 +76,5 @@ module.exports = {
   Serializador: Serializador,
   SerializadorFornecedor: SerializadorFornecedor,
   SerializadorErro: SerializadorErro,
-  formatosAceitos: ['application/json']
+  formatosAceitos: ['application/json', 'application/xml']
 };
